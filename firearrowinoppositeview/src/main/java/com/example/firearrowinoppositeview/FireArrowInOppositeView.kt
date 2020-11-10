762c7cd41838090aecda5bd334a8ded5ce846954
Anwesh43 <anweshthecool0@gmail.com>
2020-11-11 03:17:24 +0530

    adding FAIONode class

--- firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
+++ firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
@@ -131,4 +131,45 @@ class FireArrowInOppositeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FAIONode(var i : Int, val state : State = State()) {
+
+        private var next : FAIONode? = null
+        private var prev : FAIONode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FAIONode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFAIONode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FAIONode {
+            var curr : FAIONode? = prev
+            if (dir == 1) {
+               curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
