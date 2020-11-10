e566aa6a62d57b42b1989c1725c6d4dfa80fd229
Anwesh43 <anweshthecool0@gmail.com>
2020-11-11 02:52:47 +0530

    adding Canvas extension methods

--- firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
+++ firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
@@ -24,8 +24,47 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val arrowSizeFactor : Float = 4.5f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArrow(sf1 : Float, sf2 : Float, size : Float, paint : Paint) {
+    val arrowSize : Float = size / arrowSizeFactor
+    drawLine(0f, 0f, size * sf1, 0f, paint)
+    save()
+    translate(size, 0f)
+    for (j in 0..1) {
+        save()
+        rotate(45f * sf2 * (1f - 2 * j))
+        drawLine(0f, 0f, -arrowSize * sf1, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawFireArrowInOpposite(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(90f * sf.divideScale(2, parts))
+        translate(0f, h * 0.5f * sf.divideScale(3, parts))
+        drawArrow(sf.divideScale(0, parts), sf.divideScale(1, parts), size, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawFAIONode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFireArrowInOpposite(scale, w, h, paint)
+}
