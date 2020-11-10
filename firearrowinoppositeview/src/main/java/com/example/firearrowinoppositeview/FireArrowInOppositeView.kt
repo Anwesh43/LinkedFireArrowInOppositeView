2844afd7a50b8004262a322bc8844fd35130778d
Anwesh43 <anweshthecool0@gmail.com>
2020-11-11 03:28:13 +0530

    adding Renderer class

--- firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
+++ firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
@@ -195,4 +195,27 @@ class FireArrowInOppositeView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : FireArrowInOppositeView) {
+
+        private val animator : Animator = Animator(view)
+        private val faio : FireArrowInOpposite = FireArrowInOpposite(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            faio.draw(canvas, paint)
+            animator.animate {
+                faio.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            faio.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
