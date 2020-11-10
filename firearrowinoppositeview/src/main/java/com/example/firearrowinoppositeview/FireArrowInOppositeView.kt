ea27602ece6653f609ed3751083a638bce84abf0
Anwesh43 <anweshthecool0@gmail.com>
2020-11-11 02:40:54 +0530

    adding constants and libraries

--- firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
+++ firearrowinoppositeview/src/main/java/com/example/firearrowinoppositeview/FireArrowInOppositeView.kt
@@ -1,2 +1,26 @@
 package com.example.firearrowinoppositeview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#3F51B5",
+    "#4CAF50",
+    "#FFC107",
+    "#03A9F4",
+    "#F44336"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
